594b4518f4cf0e52f948e1a24be766eb2f4846e1
George Walker <george@walkersoftware.ca>
2024-04-12 10:33:02 -0700

    initial version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import {EoiFormComponent} from "./eoi-form/eoi-form.component";
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/register', pathMatch: 'full' },
+    { path: '', redirectTo: '/eoi-form', pathMatch: 'full' },
+    { path: 'eoi-form', component: EoiFormComponent },
     { path: 'register', component: RegisterPageComponent },
 ];
